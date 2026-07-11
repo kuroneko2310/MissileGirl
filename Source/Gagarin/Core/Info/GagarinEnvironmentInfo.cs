4b158449f0ad600ca5eca112544b56f7f4a059fc
kuroneko2310 <93176041+kuroneko2310@users.noreply.github.com>
2026-07-11 17:42:35 +0900

    Track mod fingerprint manifest as part of cache validity

--- Source/Gagarin/Core/Info/GagarinEnvironmentInfo.cs
+++ Source/Gagarin/Core/Info/GagarinEnvironmentInfo.cs
@@ -1,127 +1,84 @@
-﻿// // Copyright (c) 2026 ViralReaction
-// //
-// // This program and the accompanying materials are made available under the
-// // terms of the Eclipse Public License 2.0 which is available at
-// // http://www.eclipse.org/legal/epl-2.0.
-// //
-// // SPDX-License-Identifier: EPL-2.0
-
-using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Runtime.Remoting.Messaging;
 using MissileGirl;
-using Verse;
 
 namespace Gagarin
 {
     public class GagarinEnvironmentInfo
     {
         private const string CacheFolderName = "Cache";
-
         private const string TexturesFolderName = "Textures";
-
         private const string ReportsFolderName = "Reports";
-
         private const string UnifiedXmlFileName = "Unified.xml";
-
         private const string UnifiedPatchedOriginalXmlFileName = "Unified_Original.xml";
-
         private const string ModListFileName = "ModList.xml";
-
+        private const string ModFingerprintFileName = "ModFingerprint.xml";
         private const string HashFileName = "AssetsHash.xml";
-
         private const string HashIntFileName = "AssetsHashInt.xml";
-
         private const string GagarinSettingsFileName = "GagarinSettings.xml";
 
-
         private static string _cacheFolderPath;
-
-        public static string CacheFolderPath
-        {
-            get => _cacheFolderPath ?? (_cacheFolderPath = Path.Combine(RocketEnvironmentInfo.CustomConfigFolderPath, CacheFolderName));
-        }
+        public static string CacheFolderPath =>
+            _cacheFolderPath ??= Path.Combine(RocketEnvironmentInfo.CustomConfigFolderPath, CacheFolderName);
 
         private static string _gagarinSettingsPath;
-
-        public static string GagarinSettingsFilePath
-        {
-            get => _gagarinSettingsPath ?? (_gagarinSettingsPath = Path.Combine(CacheFolderPath, GagarinSettingsFileName));
-        }
+        public static string GagarinSettingsFilePath =>
+            _gagarinSettingsPath ??= Path.Combine(CacheFolderPath, GagarinSettingsFileName);
 
         private static string _texturesFolderPath;
-
-        public static string TexturesFolderPath
-        {
-            get => _texturesFolderPath ?? (_texturesFolderPath = Path.Combine(CacheFolderPath, TexturesFolderName));
-        }
+        public static string TexturesFolderPath =>
+            _texturesFolderPath ??= Path.Combine(CacheFolderPath, TexturesFolderName);
 
         private static string _reportsFolderPath;
-
-        public static string ReportsFolderPath
-        {
-            get => _reportsFolderPath ?? (_reportsFolderPath = Path.Combine(RocketEnvironmentInfo.CustomConfigFolderPath, ReportsFolderName));
-        }
+        public static string ReportsFolderPath =>
+            _reportsFolderPath ??= Path.Combine(RocketEnvironmentInfo.CustomConfigFolderPath, ReportsFolderName);
 
         private static string _unifiedXmlPath;
-
-        public static string UnifiedXmlFilePath
-        {
-            get => _unifiedXmlPath ?? (_unifiedXmlPath = Path.Combine(CacheFolderPath, UnifiedXmlFileName));
-        }
+        public static string UnifiedXmlFilePath =>
+            _unifiedXmlPath ??= Path.Combine(CacheFolderPath, UnifiedXmlFileName);
 
         private static string _unifiedPatchedOriginalXmlPath;
-
-        public static string UnifiedPatchedOriginalXmlPath
-        {
-            get => _unifiedPatchedOriginalXmlPath ?? (_unifiedPatchedOriginalXmlPath = Path.Combine(CacheFolderPath, UnifiedPatchedOriginalXmlFileName));
-        }
-
+        public static string UnifiedPatchedOriginalXmlPath =>
+            _unifiedPatchedOriginalXmlPath ??= Path.Combine(CacheFolderPath, UnifiedPatchedOriginalXmlFileName);
 
         private static string _modListPath;
+        public static string ModListFilePath =>
+            _modListPath ??= Path.Combine(CacheFolderPath, ModListFileName);
 
-        public static string ModListFilePath
-        {
-            get => _modListPath ?? (_modListPath = Path.Combine(CacheFolderPath, ModListFileName));
-        }
-
+        private static string _modFingerprintPath;
+        public static string ModFingerprintFilePath =>
+            _modFingerprintPath ??= Path.Combine(CacheFolderPath, ModFingerprintFileName);
 
         private static string _hashFilePath;
-
-        public static string HashFilePath
-        {
-            get => _hashFilePath ?? (_hashFilePath = Path.Combine(CacheFolderPath, HashFileName));
-        }
+        public static string HashFilePath =>
+            _hashFilePath ??= Path.Combine(CacheFolderPath, HashFileName);
 
         private static string _hashFilePathInt;
+        public static string HashFilePathInt =>
+            _hashFilePathInt ??= Path.Combine(CacheFolderPath, HashIntFileName);
 
-        public static string HashFilePathInt
-        {
-            get => _hashFilePathInt ?? (_hashFilePathInt = Path.Combine(CacheFolderPath, HashIntFileName));
-        }
+        public static bool CacheExists =>
+            Directory.Exists(CacheFolderPath)
+            && File.Exists(UnifiedXmlFilePath)
+            && File.Exists(HashFilePath)
+            && File.Exists(HashFilePathInt)
+            && File.Exists(ModListFilePath)
+            && File.Exists(ModFingerprintFilePath);
 
-
-        public static bool CacheExists
-        {
-            get => File.Exists(UnifiedXmlFilePath) && File.Exists(HashFilePath) && File.Exists(HashFilePathInt) && Directory.Exists(CacheFolderPath);
-        }
-
-        private static bool _modListChangedInt;
-
+        private static bool _modListChangedInitialized;
         private static bool _modListChanged;
 
         public static bool ModListChanged
         {
             get
             {
-                if (_modListChangedInt)
+                if (_modListChangedInitialized)
                     return _modListChanged;
 
-                _modListChangedInt = true;
+                _modListChangedInitialized = true;
                 _modListChanged = RunningModsSetUtility.Changed(
-                    Context.RunningMods.Select(m => m.PackageId).ToList(), ModListFilePath);
+                                      Context.RunningMods.Select(mod => mod.PackageId).ToList(), ModListFilePath)
+                                  || ModFingerprintUtility.Changed(Context.RunningMods, ModFingerprintFilePath);
                 return _modListChanged;
             }
         }
