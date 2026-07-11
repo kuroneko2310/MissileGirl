7c3024622d6598dcf0253b8facfe2ddecc681965
kuroneko2310 <93176041+kuroneko2310@users.noreply.github.com>
2026-07-11 18:24:40 +0900

    Stream asset manifests and hash XML files in one pass

--- Source/Gagarin/Core/Others/AssetHashingUtility.cs
+++ Source/Gagarin/Core/Others/AssetHashingUtility.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 using System.Xml;
@@ -13,18 +14,9 @@ namespace Gagarin
         public static Dictionary<string, string> Load(string path)
         {
             Dictionary<string, string> result = new Dictionary<string, string>();
-            XmlDocument document = new XmlDocument();
-            XmlReaderSettings settings = new XmlReaderSettings
-            {
-                IgnoreComments = true,
-                IgnoreWhitespace = true,
-                CheckCharacters = false
-            };
             try
             {
-                using StringReader input = new StringReader(File.ReadAllText(path));
-                using XmlReader xmlReader = XmlReader.Create(input, settings);
-                document.Load(xmlReader);
+                XmlDocument document = LoadDocument(path);
                 if (document.DocumentElement == null)
                     return result;
 
@@ -47,18 +39,9 @@ namespace Gagarin
         public static Dictionary<string, ulong> LoadInt(string path)
         {
             Dictionary<string, ulong> result = new Dictionary<string, ulong>();
-            XmlDocument document = new XmlDocument();
-            XmlReaderSettings settings = new XmlReaderSettings
-            {
-                IgnoreComments = true,
-                IgnoreWhitespace = true,
-                CheckCharacters = false
-            };
             try
             {
-                using StringReader input = new StringReader(File.ReadAllText(path));
-                using XmlReader xmlReader = XmlReader.Create(input, settings);
-                document.Load(xmlReader);
+                XmlDocument document = LoadDocument(path);
                 if (document.DocumentElement == null)
                     return result;
 
@@ -66,7 +49,8 @@ namespace Gagarin
                 {
                     if (node is not XmlElement element)
                         continue;
-                    result[element.GetAttribute("id")] = ulong.Parse(element.GetAttribute("hash"));
+                    if (ulong.TryParse(element.GetAttribute("hash"), out ulong hash))
+                        result[element.GetAttribute("id")] = hash;
                 }
             }
             catch (Exception exception)
@@ -82,11 +66,12 @@ namespace Gagarin
         {
             AtomicFile.Write(path, temporaryPath =>
             {
-                XmlDocument document = new XmlDocument();
+                XmlDocument document = new XmlDocument { XmlResolver = null };
                 XmlElement root = document.CreateElement("AssetsHash");
                 document.AppendChild(root);
 
-                foreach (KeyValuePair<string, T> assetHashPair in hashes)
+                foreach (KeyValuePair<string, T> assetHashPair in hashes.OrderBy(pair => pair.Key,
+                             StringComparer.Ordinal))
                 {
                     XmlElement assetXml = document.CreateElement("Asset");
                     assetXml.SetAttribute("id", assetHashPair.Key);
@@ -124,6 +109,50 @@ namespace Gagarin
                 i += lowTolerance ? 2 : 1;
             }
             return hashedValue;
+        }
+
+        public static void CalculateFileHashes(string path, out string md5Hash, out ulong contentHash)
+        {
+            const int bufferSize = 64 * 1024;
+            byte[] buffer = new byte[bufferSize];
+            ulong fnv = 1469598103934665603UL;
+
+            using MD5 md5 = MD5.Create();
+            using FileStream stream = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite,
+                bufferSize, FileOptions.SequentialScan);
+
+            int read;
+            while ((read = stream.Read(buffer, 0, buffer.Length)) > 0)
+            {
+                md5.TransformBlock(buffer, 0, read, null, 0);
+                for (int i = 0; i < read; i++)
+                {
+                    fnv ^= buffer[i];
+                    fnv *= 1099511628211UL;
+                }
+            }
+
+            md5.TransformFinalBlock(Array.Empty<byte>(), 0, 0);
+            md5Hash = BitConverter.ToString(md5.Hash ?? Array.Empty<byte>());
+            contentHash = fnv;
+        }
+
+        private static XmlDocument LoadDocument(string path)
+        {
+            XmlReaderSettings settings = new XmlReaderSettings
+            {
+                IgnoreComments = true,
+                IgnoreWhitespace = true,
+                CheckCharacters = false,
+                DtdProcessing = DtdProcessing.Prohibit,
+                XmlResolver = null
+            };
+
+            XmlDocument document = new XmlDocument { XmlResolver = null };
+            using FileStream input = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.Read);
+            using XmlReader xmlReader = XmlReader.Create(input, settings);
+            document.Load(xmlReader);
+            return document;
         }
     }
 }
